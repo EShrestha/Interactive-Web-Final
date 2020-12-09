1e9974c20e9b54d0a43a94e76984501355790cdb
wells223 <dwells@student.neumont.edu>
2020-12-08 18:25:10 -0700

    first set of changes for bar graft

--- front-end/script.js
+++ front-end/script.js
@@ -14,4 +14,26 @@ window.onload = () => {
     let q3COunt = 10;
 
     // do canvas stuff here with that data
+    const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 900;
+canvas.height = 400;
+
+context.fillStyle = "#2f6"
+context.fillRect(300, 80, 80, 40);
+
+context.strokeStyle = "#ff";
+context.strokeRect(150, 80, 80, 40);
+
+context.strokeStyle = "#f00";
+context.beginPath();
+context.moveTo(450, 10);
+context.lineTo(420, 35);
+context.lineTo(300, 60);
+
+
+
+context.font = "30px Arial";
+context.fillText("bar Graph", 50, 150);
 }
